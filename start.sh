0049c556867954dbc0bf5af4fbfb5af2f016a8d2
egaraev@gmail.com <Kax0vka44609>
2019-01-17 23:09:50 +0100

    fixed start

--- start.sh
+++ start.sh
@@ -1,28 +1,7 @@
 #!/bin/bash
 #exec &>>/var/log/work.log
 
-while true
-do
-SERVICE0='heikin_ashi.py'
 
-ps -ef | grep $SERVICE0 | grep -v grep
-[ $?  -eq "0" ] && echo "$SERVICE0 process is running" || echo "$SERVICE0 process is not running, starting"; python /usr/local/bin/heikin_ashi.py
-
-
-
-SERVICE1='heikin_day.py'
-
-ps -ef | grep $SERVICE1 | grep -v grep
-[ $?  -eq "0" ] && echo "$SERVICE1 process is running" || echo "$SERVICE1 process is not running, starting"; python /usr/local/bin/heikin_day.py
-
-
-
-
-SERVICE2='btc_ha.py'
-
-ps -ef | grep $SERVICE2 | grep -v grep
-[ $?  -eq "0" ] && echo "$SERVICE2 process is running" || echo "$SERVICE2 process is not running, starting"; python /usr/local/bin/btc_ha.py
-
-
-sleep 10
-done
+./service0.sh &
+./service1.sh &
+./service2.sh
